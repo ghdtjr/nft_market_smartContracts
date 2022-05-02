6d86514aed5c111eeb967a3d60495d0d7d0a36d3
홍석 <hongsek@MacPro.local>
2022-05-02 13:48:19 +0900

    feat: interface for ERC721

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -1,8 +1,169 @@
+// SPDX-License-Identifier: UNLICENSED 
+
 pragma solidity ^0.8.0;
 
 import "./IERC721.sol";
 import "./SafeMath.sol";
 
-contract ERC721 {
+contract ERC721 is IERC721 {
+
+    using SafeMath for uint256;
     
+    mapping (address => uint256) private _balances;
+
+    mapping (uint256 => address) private _tokenIdtoOwner;
+
+    // for approval function
+    mapping (uint256 => address) private _tokenApprovals;
+
+    mapping (uint256 => string) private _tokenURIs; 
+
+    uint256 private _tokenIdcnt = 0;
+
+    /**
+     * @dev Returns the number of tokens in ``owner``'s account.
+     */
+    function balanceOf(address owner) public view override returns (uint256 balance) {
+    }
+
+    /**
+     * @dev Returns the owner of the `tokenId` token.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function ownerOf(uint256 tokenId) public view override returns (address owner) {
+    }
+
+    function _transferFrom(address from, address to, uint256 tokenId) private {
+    }
+
+    /**
+     * @dev Safely transfers `tokenId` token from `from` to `to`.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must exist and be owned by `from`.
+     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) public override {
+    }
+
+    /**
+     * @dev Safely transfers `tokenId` token from `from` to `to`, checking first that contract recipients
+     * are aware of the ERC721 protocol to prevent tokens from being forever locked.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must exist and be owned by `from`.
+     * - If the caller is not `from`, it must be have been allowed to move this token by either {approve} or {setApprovalForAll}.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function safeTransferFrom(address from, address to, uint256 tokenId) public override {
+    }
+
+   /**
+     * @dev Transfers `tokenId` token from `from` to `to`.
+     *
+     * WARNING: Usage of this method is discouraged, use {safeTransferFrom} whenever possible.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must be owned by `from`.
+     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address from, address to, uint256 tokenId) public override {
+    }
+ 
+
+    /**
+     * @dev Gives permission to `to` to transfer `tokenId` token to another account.
+     * The approval is cleared when the token is transferred.
+     *
+     * Only a single account can be approved at a time, so approving the zero address clears previous approvals.
+     *
+     * Requirements:
+     *
+     * - The caller must own the token or be an approved operator.
+     * - `tokenId` must exist.
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address to, uint256 tokenId) public override {
+    }
+
+    /**
+     * @dev Approve or remove `operator` as an operator for the caller.
+     * Operators can call {transferFrom} or {safeTransferFrom} for any token owned by the caller.
+     *
+     * Requirements:
+     *
+     * - The `operator` cannot be the caller.
+     *
+     * Emits an {ApprovalForAll} event.
+     */
+    function setApprovalForAll(address operator, bool approved) public override {
+    }
+
+    /**
+     * @dev Returns the account approved for `tokenId` token.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function getApproved(uint256 tokenId) public view override returns (address operator) {
+    }
+
+    /**
+     * @dev Returns if the `operator` is allowed to manage all of the assets of `owner`.
+     *
+     * See {setApprovalForAll}
+     */
+    function isApprovedForAll(address owner, address operator) public view override returns (bool) {
+    }
+
+    /**
+     * @dev Mints a token to an address with a tokenURI.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     *
+     * @param _to address of the future owner of the token
+     */
+    function _mint(address _to, string memory _URI) public {
+    }
+
+    /**
+     * @dev Safely mints `tokenId` and transfers it to `to`.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must not exist.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _safeMint(address _to, uint256 _tokenId) internal virtual {
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override returns (bool){
+
+    }
+
 }
--- migrations/2_ERC721.js
+++ migrations/2_ERC721.js
@@ -1,7 +1,9 @@
 const HelloWorld = artifacts.require("HelloWorld");
+const ERC721 = artifacts.require("ERC721");
 
 module.exports = function (deployer) {
   // 첫번째 파라미터는 배포할 솔리디티 파일을 명시
   // 두번째 파라미터는 HelloWorld.sol의 생성자의 매개변수에 전달한다. 
   deployer.deploy(HelloWorld, "Hello World!!");
+  deployer.deploy(ERC721, "https://github.com/ghdtjr/pintos"); 
 };
