df56557c64e78665ae6ddf8b9351af2f9d1a930a
홍석 <hongsek@MacPro.local>
2022-05-06 13:58:14 +0900

    fix: migration constructor

--- migrations/2_ERC721.js
+++ migrations/2_ERC721.js
@@ -5,5 +5,5 @@ module.exports = function (deployer) {
   // 첫번째 파라미터는 배포할 솔리디티 파일을 명시
   // 두번째 파라미터는 HelloWorld.sol의 생성자의 매개변수에 전달한다. 
   deployer.deploy(HelloWorld, "Hello World!!");
-  deployer.deploy(ERC721, "https://github.com/ghdtjr/pintos"); 
+  deployer.deploy(ERC721, "NFT", "symbol"); 
 };
